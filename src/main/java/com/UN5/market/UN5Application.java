66a10f21c3cc903b75d08d7840d8b315a7d27149
kegonzalezs <kegonzalezs@unal.edu.co>
2021-07-04 20:35:01 -0500

    Hols kevin

--- src/main/java/com/UN5/market/UN5Application.java
+++ src/main/java/com/UN5/market/UN5Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UN5Application {
-//hola hola hola
-	//sirve?
+//hola hols
 	public static void main(String[] args) {
 		SpringApplication.run(UN5Application.class, args);
 	}
