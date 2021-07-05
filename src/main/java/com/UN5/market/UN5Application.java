f470d0faac662e2a321fef2ff43f070b9f084277
DavidHernandez2001 <72111182+DavidHernandez2001@users.noreply.github.com>
2021-07-04 20:23:34 -0500

    agregar comentario

--- src/main/java/com/UN5/market/UN5Application.java
+++ src/main/java/com/UN5/market/UN5Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UN5Application {
 //hola hola hola
+	//sirve?
 	public static void main(String[] args) {
 		SpringApplication.run(UN5Application.class, args);
 	}
